23dab5b783dca980554bee4930a96462a403acaa
kamrul hasan <mdkamrulhasanmahi@gmail.com>
2025-10-20 21:24:35 +0600

    add-next

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 
+import { Suspense } from 'react'
 import './App.css'
+import Bottels from './component/botoles/Bottels'
 
 function App() {
+  
+  const allbottels=fetch('./water.json')
+  .then(res=>res.json())
+
+
   
 
   return (
     <>
-      
-      <h1>water is the life</h1>
+    <Suspense fallback={<h1>hello ahi ...................</h1>}>
+       <Bottels allbottels={allbottels}></Bottels>
+    </Suspense>
+     
+      <div>
+        <h1></h1>
+      </div>
   
     </>
   )
---
+++ src/component/botoles/Bottels.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react';
+
+const Bottels = ({allbottels}) => {
+
+    const usedata=use(allbottels);
+    console.log(usedata);
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bottels;
